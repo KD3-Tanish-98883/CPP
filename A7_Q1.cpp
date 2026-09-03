1205c3ee8a94483d5a978bcbc5f78a75f3807df8
tanishagarwal <tanish.agarwal@kinetixtt.com>
2026-09-03 21:47:07 +0530

    Assignment

--- A7_Q1.cpp
+++ A7_Q1.cpp
@@ -1,199 +1,229 @@
 #include<iostream>
+#include<typeinfo>
 using namespace std;
+class employee{
+      private:
+      int id;
+      double salary;
 
-class Employee{
-    private:
-    int id;
-    double salary;
+      public:
+      employee(){
 
-    public:
-    Employee(void): id(0), salary(0.0)
-    {   }
+      }
+      employee(int id,double salary):id(id),salary(salary){
 
-    Employee(int id , double salary): id(id), salary(salary)
-    {   }
-
-    void setId(int id){
-        this->id = id;
-    }
-
-    int getId(){
-        return this->id;
-    }
-
-    void setSalary(double salary){
-        this->salary= salary;
-    }
-
-    double getSalary(){
-        return this->salary;
-    }
-
-    virtual void accept(){
-        cout<<"Enter Employee Id : "<<endl;
-        cin>>id;
-        cout<<"Enter Employee Salary : "<<endl;
-        cin>>salary;
-    }
-
-   virtual void display(){
-        cout<<"Employee Id : "<<id<<endl;
-        cout<<"Employee Salary : "<<salary<<endl;
-    }
+      }
+      void setId(int Id){
+           this->id=Id;
+      }
+      int getId(){
+            return this->id;
+      }
+      void setSalary(double salary){
+            this->salary=salary;
+      }
+      double getSalary(){
+            return this->salary;
+      }
+      virtual void accept(){
+            cout<<"enter id : ";
+            cin>>this->id;
+            cout<<"enter salary : ";
+            cin>>this->salary;
+      }
+      virtual void display(){
+            cout<<"ID : "<<this->id<<" Salary : "<<this->salary<<" ";
+      }
 };
-
-class Manager : virtual public Employee{
-    private:
+class manager:virtual public employee{
+    protected:
     double bonus;
 
     public:
-    Manager(): bonus(0)
-    {    }
+    manager(){
 
-    Manager(int id , double salary, double bonus): Employee(id,salary){
-        this->bonus= bonus;
     }
-
+    manager(int id ,double salary, double bonus):employee(id,salary),bonus(bonus){
+        
+    }
     void setBonus(double bonus){
-        this->bonus = bonus;
+         this->bonus=bonus;
     }
-
     double getBonus(){
-        return this->bonus;
+      return this->bonus;
     }
-
     void accept(){
-        Employee::accept();
-        acceptManager();
+      employee::accept();
+      acceptManager();
     }
-
     void display(){
-        Employee::display();
-        displayManager();
+      employee::display();
+      displayManager();
     }
 
     protected:
     void acceptManager(){
-        Employee::accept();
-        cout<<"Enter Bonus : "<<endl;
-        cin>>bonus;
+      cout<<"enter bonus : ";
+      cin>>this->bonus;
+      //cout<<endl;
     }
-
     void displayManager(){
-        
-        cout<<"Bonus : "<<bonus<<endl;
+      cout<<"Bonus : "<<this->bonus<<endl;
     }
 };
-
-class Salesman : virtual public Employee{
-    private:
-    double commission;
+class salesman:virtual public employee{
+    protected:
+    double commision;
 
     public:
-    Salesman(): commission(0)
-    {    }
+    salesman(){
 
-    Salesman(int id , double salary, double commission): Employee(id,salary){
-        this->commission= commission;
     }
-
-    void setCommission(double commission){
-        this->commission = commission;
+    salesman(int id ,double salary, double commision):employee(id,salary),commision(commision){
+        
     }
-
-    double getCommission(){
-        return this-> commission;
+    void setcommision(double commision){
+         this->commision=commision;
     }
-
-    virtual void accept(){
-        Employee::accept();
-        acceptSalesman();
+    double getcommision(){
+      return this->commision;
     }
-
-    virtual void display(){
-        Employee::display();
-        displaySalesman();
+    void accept(){
+      employee::accept();
+      acceptSalesman();
     }
-
+    void display(){
+      employee::display();
+      displaySalesman();
+    }
+    
     protected:
     void acceptSalesman(){
-        cout<<"Enter the commission : "<<endl;
-        cin>>commission;
+      cout<<"enter commision : ";
+      cin>>this->commision;
+      //cout<<endl;
     }
-
     void displaySalesman(){
-        cout<<"Commission : "<<commission<<endl;
+      cout<<" Commision : "<<this->commision<<endl;
     }
 };
-
-class SalesManager : public Manager, public Salesman{
+class salesManager:public manager,public salesman{
     public:
-    SalesManager()
-    {   }
-    SalesManager(int id, double salary, double bonus ,double commission):Manager(id,salary, bonus),Salesman(id, salary, commission)
-    {   }
+    salesManager(){
 
-    void accept(){
-        Employee::accept();
-        Manager::acceptManager();
-        Salesman::acceptSalesman();
     }
+    salesManager(int id,double salary,double bonus,double commision):manager(id,salary,bonus),salesman(id,salary,commision){
 
+    }
+    void accept(){
+      //employee::accept(); this is amguity. solved using virtual
+      employee::accept();
+      manager::acceptManager();
+      salesman::acceptSalesman();
+    }
     void display(){
-        Employee::accept();
-        Manager::display();
-        Salesman::display();
+      employee::display();
+      manager::displayManager();
+      salesman::displaySalesman();
     }
 };
 
-int menuList(){
-    int choice;
-    cout<<"1. Add Manager "<<endl;
-    cout<<"2. Add Salesman "<<endl;
-    cout<<"3. Add Salesmanager "<<endl;
+int menu(){
+    cout<<"0. Exit"<<endl;
+    cout<<"1. Add Manager"<<endl;
+    cout<<"2. Add Salesman"<<endl;
+    cout<<"3. Add Salesmanager"<<endl;
     cout<<"4. Display the count of all employees with respect to designation"<<endl;
     cout<<"5. Display All Managers"<<endl;
     cout<<"6. Display All Salesman"<<endl;
     cout<<"7. Display All SalesManagers"<<endl;
-    cin>>choice;
-    return choice;
+    int x;
+    cin>>x;
+    return x;
 }
-
 int main(){
+    employee* ptr[5];
     int choice;
-    SalesManager *sm[5];
+    int i=0;
+    while((choice=menu())!=0){
+        switch(choice){
+            case 1:
+              if(i<5){
+                ptr[i]=new manager;
+                ptr[i]->accept();
+                i++;
+              }
+              else{
+                cout<<"limit exceeds ";
+              }
+              break;
 
-    for(int i=0; i<=5; i++)
-    {
-    switch (::menuList())
-    {
-        case 1:
-        {
-            sm[i]->Manager::accept();
-            break;
-        }
-        case 2:{
-            sm[i]->Salesman::accept();
-            break;
-        }
-        case 3:{
-            sm[i]->SalesManager::accept();
-            break;
-        }
-        case 4:{
+            case 2:
+              if(i<5){
+              ptr[i]=new salesman;
+              ptr[i]->accept();
+              i++;
+              }
+              else{
+                cout<<"limit exceeds ";
+              }
+              break;
 
-        }
-        case 5:{
-            
-        }
-        case 6:{
-            
-        }
-        case 7:{
+            case 3:
+              if(i<5){
+              ptr[i]=new salesManager;
+              ptr[i]->accept();
+              i++;
+              }
+              else{
+              cout<<"limit exceeds ";
+              }
+              break;
 
-        }
-        default:
-        break;
+            case 4:{
+              int m=0;
+              int s=0;
+              int sm=0;
+              for(int j=0;j<i;j++){
+                  if(typeid(*ptr[j])==typeid(manager)){
+                    m++;
+                  }
+                  else if(typeid(*ptr[j])==typeid(salesman)){
+                    s++;
+                  }
+                  else if(typeid(*ptr[j])==typeid(salesManager)){
+                    sm++;
+                  }
+              }
+              cout<<m<<" managers"<<endl;
+              cout<<s<<" salesman"<<endl;
+              cout<<sm<<" salesmanager"<<endl;
+              break;
+            }
+              
+            case 5:
+              for(int j=0;j<i;j++){
+                if(ptr[j]!=nullptr && typeid(*ptr[j])==typeid(manager)){
+                  ptr[j]->display();
+                }
+              }
+              break;
+
+            case 6:
+              for(int j=0;j<i;j++){
+                if(ptr[j]!=nullptr && typeid(*ptr[j])==typeid(salesman)){
+                  ptr[j]->display();
+                }
+              }
+              break;
+
+            case 7:
+              for(int j=0;j<i;j++){
+                if(ptr[j]!=nullptr && typeid(*ptr[j])==typeid(salesManager)){
+                  ptr[j]->display();
+                }
+              }
+              break;
         }
     }
 }
